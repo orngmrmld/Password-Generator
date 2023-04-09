e935a88461bd23a9d77f23468f561ccb7b663099
Vagabond <marcusjjreyes@gmail.com>
2023-04-09 09:24:06 -0700

    added length check criteria

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Global Variables I wrote
+var includeLow = false;
+var includeUp = false;
+var includeNum = false;
+var includeSpecChar = false;
+var passLength = 0;
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const specialChar = '!()-.?[]`;:#$^&*+='
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +17,16 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// Function that generates a string
+function generateWord(length){
+  let word = ' ';
+
+  for (let i = 0; i < length; i++){
+    word += characters.charAt(Math.floor(Math.random()*characters.length));
+  }
+  return word;
 }
 
 // Add event listener to generate button
@@ -18,14 +37,25 @@ generateBtn.addEventListener("click", writePassword);
 
 // Prompt shows two criteria: length and character type
 
-
-
-
-
-
-
 // Criteria 1: Length
 // let useser choose legnth of password between 8 and 128
+// when function is called, a prompt appears asking for a number between 8 and 128
+// check if the entered number is between 8 and 128 if not try again
+// convert input from string to a number
+function lengthCheck(){
+  passLength = window.prompt("Enter a number between 8 and 128");
+  passLength = parseInt(passLength);
+  if (passLength < 8 || passLength > 128){
+    window.alert("Pick a number between 8 and 128 Please!");
+    passLength = 0;
+    return;
+  } else if (isNaN(passLength)){
+    window.alert("Pick a number please!");
+    passLength = 0;
+    return;
+  }
+
+}
 
 
 // Criteria 2: Character Type
