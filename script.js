188e067f0cd78b509540a24a142507ab349e169d
Vagabond <marcusjjreyes@gmail.com>
2023-04-09 13:52:43 -0700

    finished first working proto

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const integers = '0123456789'
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(passLength);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -20,8 +20,22 @@ function writePassword() {
 }
 
 // Function that generates a string
-function generatePassword(length,charType){
+// Default password is 8 char in length and does not use special characters
+function generatePassword(length){
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  if(counter < 0)
+  characters = ''
   let word = ' ';
+
+  if(includeLow)
+  characters += lower;
+  if(includeUp)
+  characters += upper;
+  if(includeSpecChar)
+  characters += special;
+  if(includeNum)
+  characters += integers;
+
 
   for (let i = 0; i < length; i++){
     word += characters.charAt(Math.floor(Math.random()*characters.length));
@@ -31,7 +45,7 @@ function generatePassword(length,charType){
 
 // Add event listener to generate button
 // When the button is clicked a prompt for password criteria shows up
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePass);
 
 
 
@@ -40,20 +54,25 @@ generateBtn.addEventListener("click", writePassword);
 // first prompt asks if user wants to select the length (yes or no) (no defaults to passLength 8)
 // second prompt asks if user want it to be all upper case (1) all lower case (2) or include both (3)
 // third prompt asks if user wants to include numbers (1) special characters (2) or include both (3)
-function writePassword(event){
+function writePass(event){
   let choice1 = window.prompt("Enter a Number to generate a password based on length or content type:" + "\n\nEnter (1) to generate based on length" + "\nEnter (2) to generate based on content type" + "\nEnter (3) to generate based on both");
   event.preventDefault();
   if(choice1 === '1'){
     window.alert("You chose to generate based on length");
     lengthCheck();
-
+    writePassword(passLength);
   }else if(choice1 === '2'){
     window.alert("You chose to generate based on content type");
     typeCheck();
+    writePass(8);
   }else if(choice1 === '3'){
-
+    alert("You chose the most secure option! Both!")
+    lengthCheck();
+    typeCheck();
+    writePassword(passLength);
   }else{
-
+    alert("Choose a proper option");
+    writePass();
   }
 
 }
@@ -71,6 +90,7 @@ function lengthCheck(){
   if (passLength < 8 || passLength > 128){
     window.alert("Pick a number between 8 and 128 Please!");
     passLength = 0;
+    lengthCheck();
     return;
   } else if (isNaN(passLength)){
     window.alert("Pick a number please!");
@@ -79,7 +99,7 @@ function lengthCheck(){
   } else {
     alert("You chose a length of " + passLength + " characters for your password.");
   }
-
+    writePassword();
 }
 
 
@@ -87,6 +107,8 @@ function lengthCheck(){
 // shows list of character types to choose to use
 // needs to validate input and choose at least one character type
 function typeCheck(){
+  counter = 0;
+
   alert("You will be prompted to choose which character type to include, you must choose at least 1");
   let choice2 = prompt("Would you like to include lowercase characters?" + "\nYes or No?");
   choice2 = choice2.toLowerCase();
@@ -150,3 +172,4 @@ function typeCheck(){
   }
 
 }
+
