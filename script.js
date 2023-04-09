3e11f542eaf1afcb2977495e2a1cafbb64ff7058
Vagabond <marcusjjreyes@gmail.com>
2023-04-09 08:03:40 -0700

    psuedo code framework

--- script.js
+++ script.js
@@ -11,4 +11,24 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// When the button is clicked a prompt for password criteria shows up
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Prompt shows two criteria: length and character type
+
+
+
+
+
+
+
+// Criteria 1: Length
+// let useser choose legnth of password between 8 and 128
+
+
+// Criteria 2: Character Type
+// shows list of character types to choose to use
+// needs to validate input and choose at least one character type
+
